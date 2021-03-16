34cf71319889499b619681744f9e545eaeb43bf1
luyang <luyang>
2021-03-16 13:10:49 +0800

    add onedark theme for xfce4 terminal

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ ln -s `pwd`/i3 $HOME/.config/i3
 
 ## rofi
 ln -s `pwd`/rofi $HOME/.config/rofi
+
+## xfce4 terminal theme
+sudo ln -s `pwd`/onedark.theme /usr/share/xfce4/terminal/colorschemes/onedark.theme
