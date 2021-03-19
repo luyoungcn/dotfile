38e7416c504460bf3868d53535f505b914910f53
luyang <luyang>
2021-03-19 23:14:14 +0800

    gtk config

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ ln -s `pwd`/rofi $HOME/.config/rofi
 
 ## xfce4 terminal theme
 sudo ln -s `pwd`/onedark.theme /usr/share/xfce4/terminal/colorschemes/onedark.theme
+
+## gtkrc
+ln -s `pwd`/gtkrc-2.0 $HOME/.gtkrc-2.0
