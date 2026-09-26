b120cc27f980d590a01f93baa196b674aec493fd
luyang <luyang>
2026-09-26 22:05:50 +0800

    fish: source OrbStack shell integration

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,7 @@
 if status is-interactive; and type -q starship
     starship init fish | source
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
